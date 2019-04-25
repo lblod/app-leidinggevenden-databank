ca6075b9e47f28a13dfe0e43c0c3da796f6562dd
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-04-25 16:12:46 +0200

    update dispatcher to use cache

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -12,14 +12,14 @@ defmodule Dispatcher do
   plug :match
   plug :dispatch
 
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule.
+  # In order to forward the 'themes' cache to the
+  # cache service, use the following forward rule.
   #
   # docker-compose stop; docker-compose rm; docker-compose up
   # after altering this file.
   #
   # match "/themes/*path" do
-  #   Proxy.forward conn, path, "http://resource/themes/"
+  #   Proxy.forward conn, path, "http://cache/themes/"
   # end
 
   options _ do
@@ -27,158 +27,158 @@ defmodule Dispatcher do
   end
 
   match "/agendapunten/*path" do
-    Proxy.forward conn, path, "http://resource/agendapunten/"
+    Proxy.forward conn, path, "http://cache/agendapunten/"
   end
   match "/artikels/*path" do
-    Proxy.forward conn, path, "http://resource/artikels/"
+    Proxy.forward conn, path, "http://cache/artikels/"
   end
   match "/behandelingen-van-agendapunten/*path" do
-    Proxy.forward conn, path, "http://resource/behandelingen-van-agendapunten/"
+    Proxy.forward conn, path, "http://cache/behandelingen-van-agendapunten/"
   end
   match "/besluiten/*path" do
-    Proxy.forward conn, path, "http://resource/besluiten/"
+    Proxy.forward conn, path, "http://cache/besluiten/"
   end
   match "/bestuurseenheden/*path" do
-    Proxy.forward conn, path, "http://resource/bestuurseenheden/"
+    Proxy.forward conn, path, "http://cache/bestuurseenheden/"
   end
   match "/werkingsgebieden/*path" do
-    Proxy.forward conn, path, "http://resource/werkingsgebieden/"
+    Proxy.forward conn, path, "http://cache/werkingsgebieden/"
   end
   match "/bestuurseenheid-classificatie-codes/*path" do
-    Proxy.forward conn, path, "http://resource/bestuurseenheid-classificatie-codes/"
+    Proxy.forward conn, path, "http://cache/bestuurseenheid-classificatie-codes/"
   end
   match "/bestuursorganen/*path" do
-    Proxy.forward conn, path, "http://resource/bestuursorganen/"
+    Proxy.forward conn, path, "http://cache/bestuursorganen/"
   end
   match "/bestuursorgaan-classificatie-codes/*path" do
-    Proxy.forward conn, path, "http://resource/bestuursorgaan-classificatie-codes/"
+    Proxy.forward conn, path, "http://cache/bestuursorgaan-classificatie-codes/"
   end
   match "/rechtsgronden-besluit/*path" do
-    Proxy.forward conn, path, "http://resource/rechtsgronden-besluit/"
+    Proxy.forward conn, path, "http://cache/rechtsgronden-besluit/"
   end
   match "/rechtsgronden-artikel/*path" do
-    Proxy.forward conn, path, "http://resource/rechtsgronden-artikel/"
+    Proxy.forward conn, path, "http://cache/rechtsgronden-artikel/"
   end
   match "/stemmingen/*path" do
-    Proxy.forward conn, path, "http://resource/stemmingen/"
+    Proxy.forward conn, path, "http://cache/stemmingen/"
   end
   match "/zittingen/*path" do
-    Proxy.forward conn, path, "http://resource/zittingen/"
+    Proxy.forward conn, path, "http://cache/zittingen/"
   end
   match "/versioned-agendas/*path" do
-    Proxy.forward conn, path, "http://resource/versioned-agendas/"
+    Proxy.forward conn, path, "http://cache/versioned-agendas/"
   end
   match "/versioned-besluiten-lijsten/*path" do
-    Proxy.forward conn, path, "http://resource/versioned-besluiten-lijsten/"
+    Proxy.forward conn, path, "http://cache/versioned-besluiten-lijsten/"
   end
   match "/versioned-notulen/*path" do
-    Proxy.forward conn, path, "http://resource/versioned-notulen/"
+    Proxy.forward conn, path, "http://cache/versioned-notulen/"
   end
   match "/versioned-behandelingen/*path" do
-    Proxy.forward conn, path, "http://resource/versioned-behandelingen/"
+    Proxy.forward conn, path, "http://cache/versioned-behandelingen/"
   end
-  match "/signed-resources/*path" do
-    Proxy.forward conn, path, "http://resource/signed-resources/"
+  match "/signed-caches/*path" do
+    Proxy.forward conn, path, "http://cache/signed-caches/"
   end
-  match "/published-resources/*path" do
-    Proxy.forward conn, path, "http://resource/published-resources/"
+  match "/published-caches/*path" do
+    Proxy.forward conn, path, "http://cache/published-caches/"
   end
   match "/blockchain-statuses/*path" do
-    Proxy.forward conn, path, "http://resource/blockchain-statuses/"
+    Proxy.forward conn, path, "http://cache/blockchain-statuses/"
   end
   match "/notulen/*path" do
-    Proxy.forward conn, path, "http://resource/notulen/"
+    Proxy.forward conn, path, "http://cache/notulen/"
   end
   match "/agendas/*path" do
-    Proxy.forward conn, path, "http://resource/agendas/"
+    Proxy.forward conn, path, "http://cache/agendas/"
   end
   match "/uittreksels/*path" do
-    Proxy.forward conn, path, "http://resource/uittreksels/"
+    Proxy.forward conn, path, "http://cache/uittreksels/"
   end
   match "/besluitenlijsten/*path" do
-    Proxy.forward conn, path, "http://resource/besluitenlijsten/"
+    Proxy.forward conn, path, "http://cache/besluitenlijsten/"
   end
   match "/bestuursfuncties/*path" do
-    Proxy.forward conn, path, "http://resource/bestuursfuncties/"
+    Proxy.forward conn, path, "http://cache/bestuursfuncties/"
   end
   match "/functionarissen/*path" do
-    Proxy.forward conn, path, "http://resource/functionarissen/"
+    Proxy.forward conn, path, "http://cache/functionarissen/"
   end
   match "/contact-punten/*path" do
-    Proxy.forward conn, path, "http://resource/contact-punten/"
+    Proxy.forward conn, path, "http://cache/contact-punten/"
   end
   match "/mandataris-status-codes/*path" do
-    Proxy.forward conn, path, "http://resource/mandataris-status-codes/"
+    Proxy.forward conn, path, "http://cache/mandataris-status-codes/"
   end
   match "/fracties/*path" do
-    Proxy.forward conn, path, "http://resource/fracties/"
+    Proxy.forward conn, path, "http://cache/fracties/"
   end
   match "/fractietypes/*path" do
-    Proxy.forward conn, path, "http://resource/fractietypes/"
+    Proxy.forward conn, path, "http://cache/fractietypes/"
   end
   match "/geboortes/*path" do
-    Proxy.forward conn, path, "http://resource/geboortes/"
+    Proxy.forward conn, path, "http://cache/geboortes/"
   end
   match "/lijsttypes/*path" do
-    Proxy.forward conn, path, "http://resource/lijsttypes/"
+    Proxy.forward conn, path, "http://cache/lijsttypes/"
   end
   match "/kandidatenlijsten/*path" do
-    Proxy.forward conn, path, "http://resource/kandidatenlijsten/"
+    Proxy.forward conn, path, "http://cache/kandidatenlijsten/"
   end
   match "/lidmaatschappen/*path" do
-    Proxy.forward conn, path, "http://resource/lidmaatschappen/"
+    Proxy.forward conn, path, "http://cache/lidmaatschappen/"
   end
   match "/mandaten/*path" do
-    Proxy.forward conn, path, "http://resource/mandaten/"
+    Proxy.forward conn, path, "http://cache/mandaten/"
   end
   match "/bestuursfunctie-codes/*path" do
-    Proxy.forward conn, path, "http://resource/bestuursfunctie-codes/"
+    Proxy.forward conn, path, "http://cache/bestuursfunctie-codes/"
   end
   match "/mandatarissen/*path" do
-    Proxy.forward conn, path, "http://resource/mandatarissen/"
+    Proxy.forward conn, path, "http://cache/mandatarissen/"
   end
   match "/mandataris-status-codes/*path" do
-    Proxy.forward conn, path, "http://resource/mandataris-status-codes/"
+    Proxy.forward conn, path, "http://cache/mandataris-status-codes/"
   end
   match "/beleidsdomein-codes/*path" do
-    Proxy.forward conn, path, "http://resource/beleidsdomein-codes/"
+    Proxy.forward conn, path, "http://cache/beleidsdomein-codes/"
   end
   match "/personen/*path" do
-    Proxy.forward conn, path, "http://resource/personen/"
+    Proxy.forward conn, path, "http://cache/personen/"
   end
   match "/geslacht-codes/*path" do
-    Proxy.forward conn, path, "http://resource/geslacht-codes/"
+    Proxy.forward conn, path, "http://cache/geslacht-codes/"
   end
   match "/identificatoren/*path" do
-    Proxy.forward conn, path, "http://resource/identificatoren/"
+    Proxy.forward conn, path, "http://cache/identificatoren/"
   end
   match "/rechtsgronden/*path" do
-    Proxy.forward conn, path, "http://resource/rechtsgronden/"
+    Proxy.forward conn, path, "http://cache/rechtsgronden/"
   end
   match "/rechtsgronden-aanstelling/*path" do
-    Proxy.forward conn, path, "http://resource/rechtsgronden-aanstelling/"
+    Proxy.forward conn, path, "http://cache/rechtsgronden-aanstelling/"
   end
   match "/rechtsgronden-beeindiging/*path" do
-    Proxy.forward conn, path, "http://resource/rechtsgronden-beeindiging/"
+    Proxy.forward conn, path, "http://cache/rechtsgronden-beeindiging/"
   end
   match "/rechtstreekse-verkiezingen/*path" do
-    Proxy.forward conn, path, "http://resource/rechtstreekse-verkiezingen/"
+    Proxy.forward conn, path, "http://cache/rechtstreekse-verkiezingen/"
   end
   match "/tijdsgebonden-entiteiten/*path" do
-    Proxy.forward conn, path, "http://resource/tijdsgebonden-entiteiten/"
+    Proxy.forward conn, path, "http://cache/tijdsgebonden-entiteiten/"
   end
   match "/tijdsintervallen/*path" do
-    Proxy.forward conn, path, "http://resource/tijdsintervallen/"
+    Proxy.forward conn, path, "http://cache/tijdsintervallen/"
   end
   match "/verkiezingsresultaten/*path" do
-    Proxy.forward conn, path, "http://resource/verkiezingsresultaten/"
+    Proxy.forward conn, path, "http://cache/verkiezingsresultaten/"
   end
   match "/verkiezingsresultaat-gevolg-codes/*path" do
-    Proxy.forward conn, path, "http://resource/verkiezingsresultaat-gevolg-codes/"
+    Proxy.forward conn, path, "http://cache/verkiezingsresultaat-gevolg-codes/"
   end
 
   # get "/exports/*path" do
-  #   # we bypass the cache on purpose since mu-cl-resources is not the master of the exports
+  #   # we bypass the cache on purpose since mu-cl-caches is not the master of the exports
   #   Proxy.forward conn, path, "http://resource/exports/"
   # end
 
