d4667121c8cea407699a6ee35f6dfd7e6622bee0
Niels Vandekeybus <progster@gmail.com>
2019-07-08 16:25:57 +0200

    add resource config for exports

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -13,3 +13,12 @@
 (read-domain-file "slave-leidinggevenden-domain.lisp")
 (read-domain-file "slave-mandaat-domain.lisp")
 (read-domain-file "slave-publicatie-gn-domain.lisp")
+
+(define-resource export ()
+  :class (s-prefix "export:Export")
+  :properties `((:filename :string ,(s-prefix "nfo:filename"))
+                (:format :string ,(s-prefix "dct:format"))
+                (:filesize :number ,(s-prefix "nfo:fileSize"))
+                (:created :datetime ,(s-prefix "dct:created")))
+  :resource-base (s-url "http://data.lblod.info/id/exports/")
+  :on-path "exports")
