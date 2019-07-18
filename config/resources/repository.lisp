ef9093d09992e735d2c077270ab56284abd6dd88
Erika Pauwels <erika.pauwels@gmail.com>
2019-07-18 21:58:13 +0200

    Update mu-cl-resources prefixes

--- config/resources/repository.lisp
+++ config/resources/repository.lisp
@@ -35,3 +35,5 @@
 
 (add-prefix "email" "http://mu.semte.ch/vocabularies/ext/email/")
 (add-prefix "lblodlg" "http://data.lblod.info/vocabularies/leidinggevenden/")
+(add-prefix "locn" "http://www.w3.org/ns/locn#")
+(add-prefix "adres" "https://data.vlaanderen.be/ns/adres#")
