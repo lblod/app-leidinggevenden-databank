f5952aa55769fde6e12bf274698eb1f37e7c6fae
felix <ruiz.felixruiz@gmail.com>
2021-08-17 17:18:54 +0200

    added missing delta rules

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -13,6 +13,46 @@ export default [
       ignoreFromSelf: true
     }
   },
+ {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+      },
+      object: {
+        type: 'uri',
+        value: 'http://redpencil.data.gift/vocabularies/deltas/Error'
+      }
+    },
+    callback: {
+      url: 'http://delta-report-generator/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [ "http://redpencil.data.gift/id/jobs/concept/JobOperation/deltas/consumer/initialSync/leidinggevenden" ]
+    }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status'
+      }
+    },
+    callback: {
+      url: 'http://delta-report-generator/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true,
+      optOutMuScopeIds: [ "http://redpencil.data.gift/id/jobs/concept/JobOperation/deltas/consumer/initialSync/leidinggevenden" ]
+    }
+  },
   {
     match: {
       // anything
